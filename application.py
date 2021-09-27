239825117722233558981456708f14a3b34020d7
Ilya <ilya.panshenskov@italki.com>
2021-09-27 10:00:58 +0800

    adding pickled files

--- application.py
+++ application.py
@@ -1,22 +1,19 @@
 from flask import Flask, request
 import joblib
 
-vectorizer = joblib.load("/Users/ilya/Desktop/MyFlask/vectorizer.pkl")
-spamorham_model = joblib.load("/Users/ilya/Desktop/MyFlask/spam_ham_model.pkl")
+vectorizer = joblib.load("/Users/ilya/Desktop/Computer-Science/Github/mbti-classifier/tfidf_vectorizer.pkl")
+spamorham_model = joblib.load("/Users/ilya/Desktop/Computer-Science/Github/mbti-classifier/mbti_model.pkl")
 
-application = Flask(__name__)
+app = Flask(__name__)
 
-@application.route('/')
-def hello_world():
-    return "Hello World!"
-
-@application.route('/spamorham', methods = ['GET', 'POST'])
+@app.route('/', methods = ['GET', 'POST'])
 def spamorham():
     message = request.args.get("message")
-    vect_message = vectorizer.transform([message])
-    result = spamorham_model.predict(vect_message)[0]
-    return result
+    return message
+#    vect_message = vectorizer.transform([message])
+#    result = spamorham_model.predict(vect_message)[0]
+#    return result   
 
 if __name__  == '__main__':
-    application.run()
+    app.run()
 
