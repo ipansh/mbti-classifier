bd9384c7e2e2fed76b3109ac87d5af47022c2223
Ilya <ilya.panshenskov@italki.com>
2021-09-28 09:48:31 +0800

    removing redundant pickles

--- application.py
+++ application.py
@@ -2,18 +2,16 @@ from flask import Flask, request
 import joblib
 
 vectorizer = joblib.load("/Users/ilya/Desktop/Computer-Science/Github/mbti-classifier/tfidf_vectorizer.pkl")
-spamorham_model = joblib.load("/Users/ilya/Desktop/Computer-Science/Github/mbti-classifier/mbti_model.pkl")
+mbti_model = joblib.load("/Users/ilya/Desktop/Computer-Science/Github/mbti-classifier/mbti_model.pkl")
 
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET', 'POST'])
 def spamorham():
     message = request.args.get("message")
-    return message
-#    vect_message = vectorizer.transform([message])
-#    result = spamorham_model.predict(vect_message)[0]
-#    return result   
+    vect_message = vectorizer.transform([message])
+    result = mbti_model.predict(vect_message)[0]
+    return result
 
 if __name__  == '__main__':
-    app.run()
-
+    app.run()   
